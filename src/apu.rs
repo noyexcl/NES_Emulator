922035b8bad7f6610b255d2ba9a302238082a838
Noy! <78952684+noyexcl@users.noreply.github.com>
2025-06-17 22:29:07 +0900

    Fix audio delay (#5)
    Sample every 40.52 cycles instead of 40 cycles.
    
    60 fps and 44100 sampling rate need 735 samples per frame.
    
    40 cycles made 745 samples per frame, and it caused audio delay
    by 15 extra samples that couldn't be counsumed in a frame.

--- src/apu.rs
+++ src/apu.rs
@@ -14,13 +14,12 @@ mod triangle;
 use std::rc::Rc;
 
 use dmc::DMC;
-use filter::FirstOrderFilter;
 use frame_counter::{FrameCounter, FrameType};
 use noise::Noise;
 use pulse::Pulse;
 use triangle::Triangle;
 
-use crate::{rom::Rom, trace::Inspector};
+use crate::{apu::filter::FilterChain, rom::Rom, trace::Inspector};
 
 #[allow(clippy::upper_case_acronyms)]
 #[derive(Debug)]
@@ -32,13 +31,18 @@ pub struct APU {
     noise: Noise,
     dmc: DMC,
     frame_counter: FrameCounter,
-    filters: [FirstOrderFilter; 3],
     cycles: usize,
     pub cpu_stall: usize,
+    filter_chain: FilterChain,
+    sample_period: f64,
+    sample_counter: f64,
 }
 
 impl APU {
     pub fn new(rom: Rc<Rom>) -> Self {
+        //let sample_period = 1_789_773.0 / 44_100.0;
+        let sample_period = 40.52;
+
         Self {
             buffer: vec![],
             pulse1: Pulse::new(true),
@@ -47,13 +51,11 @@ impl APU {
             noise: Noise::new(),
             dmc: DMC::new(rom),
             frame_counter: FrameCounter::new(),
-            filters: [
-                FirstOrderFilter::high_pass(44100.0, 90.0),
-                FirstOrderFilter::high_pass(44100.0, 440.0),
-                FirstOrderFilter::low_pass(44100.0, 14_000.0),
-            ],
+            filter_chain: FilterChain::new(44100.0),
             cycles: 0,
             cpu_stall: 0,
+            sample_period,
+            sample_counter: sample_period,
         }
     }
 
@@ -84,14 +86,7 @@ impl APU {
         self.triangle.length_counter.reload();
         self.noise.length_counter.reload();
 
-        // We need 730 stereo audio samples per frame for 60 fps.
-        // Each frame lasts a minimum of 29,779 CPU cycles. This
-        // works out to around 40 CPU cycles per sample.
-        if self.cycles % 40 == 0 {
-            let s = self.sample();
-            self.buffer.push(s);
-            self.buffer.push(s);
-        }
+        self.sample();
 
         self.cpu_stall = self.dmc.cpu_stall;
         self.dmc.cpu_stall = 0;
@@ -118,24 +113,29 @@ impl APU {
         }
     }
 
-    pub fn sample(&mut self) -> i16 {
-        let pulse1 = self.pulse1.sample() as f64;
-        let pulse2 = self.pulse2.sample() as f64;
-        let t = self.triangle.sample() as f64;
-        let n = self.noise.sample() as f64;
-        let d = self.dmc.sample() as f64;
+    pub fn sample(&mut self) {
+        let pulse1 = self.pulse1.sample() as f32;
+        let pulse2 = self.pulse2.sample() as f32;
+        let t = self.triangle.sample() as f32;
+        let n = self.noise.sample() as f32;
+        let d = self.dmc.sample() as f32;
 
         let pulse_out = 95.88 / ((8218.0 / (pulse1 + pulse2)) + 100.0);
         let tnd_out = 159.79 / ((1.0 / (t / 8227.0 + n / 12241.0 + d / 22638.0)) + 100.0);
 
-        let mut output = (pulse_out + tnd_out) * 65535.0;
+        let output = pulse_out + tnd_out;
 
-        for i in 0..3 {
-            output = self.filters[i].tick(output);
+        self.filter_chain.consume(output);
+        self.sample_counter -= 1.0;
+
+        if self.sample_counter <= 1.0 {
+            // The final range is -32767 to +32767
+            let sample = (self.filter_chain.output() * 65535.0) as i16;
+            self.buffer.push(sample);
+            self.buffer.push(sample);
+
+            self.sample_counter += self.sample_period;
         }
-
-        // The final range is -32767 to +32767
-        output as i16
     }
 
     pub fn write_register(&mut self, addr: u16, val: u8) {
@@ -247,3 +247,51 @@ impl Inspector for APU {
 pub struct Sound {
     pub buffer: Vec<i16>,
 }
+
+#[rustfmt::skip]
+pub static PULSE_TABLE: [f32; 31] = [
+    0.0,          0.011_609_139, 0.022_939_48, 0.034_000_948, 0.044_803,    0.055_354_66,
+    0.065_664_53, 0.075_740_82,  0.085_591_4,  0.095_223_75,  0.104_645_04, 0.113_862_15,
+    0.122_881_64, 0.131_709_8,   0.140_352_64, 0.148_815_96,  0.157_105_25, 0.165_225_88,
+    0.173_182_92, 0.180_981_26,  0.188_625_59, 0.196_120_46,  0.203_470_17, 0.210_678_94,
+    0.217_750_76, 0.224_689_5,   0.231_498_87, 0.238_182_47,  0.244_743_78, 0.251_186_07,
+    0.257_512_57,
+];
+
+#[rustfmt::skip]
+pub static TND_TABLE: [f32; 203] = [
+    0.0,           0.006_699_824, 0.013_345_02,  0.019_936_256, 0.026_474_18,  0.032_959_443,
+    0.039_392_676, 0.045_774_5,   0.052_105_535, 0.058_386_38,  0.064_617_634, 0.070_799_87,
+    0.076_933_69,  0.083_019_62,  0.089_058_26,  0.095_050_134, 0.100_995_794, 0.106_895_77,
+    0.112_750_58,  0.118_560_754, 0.124_326_79,  0.130_049_18,  0.135_728_45,  0.141_365_05,
+    0.146_959_5,   0.152_512_22,  0.158_023_7,   0.163_494_4,   0.168_924_76,  0.174_315_24,
+    0.179_666_28,  0.184_978_3,   0.190_251_74,  0.195_486_98,  0.200_684_47,  0.205_844_63,
+    0.210_967_81,  0.216_054_44,  0.221_104_92,  0.226_119_6,   0.231_098_88,  0.236_043_11,
+    0.240_952_72,  0.245_828_,    0.250_669_36,  0.255_477_1,   0.260_251_64,  0.264_993_28,
+    0.269_702_37,  0.274_379_22,  0.279_024_18,  0.283_637_58,  0.288_219_72,  0.292_770_95,
+    0.297_291_52,  0.301_781_8,   0.306_242_1,   0.310_672_67,  0.315_073_85,  0.319_445_88,
+    0.323_789_12,  0.328_103_78,  0.332_390_2,   0.336_648_6,   0.340_879_3,   0.345_082_55,
+    0.349_258_63,  0.353_407_77,  0.357_530_27,  0.361_626_36,  0.365_696_34,  0.369_740_37,
+    0.373_758_76,  0.377_751_74,  0.381_719_56,  0.385_662_44,  0.389_580_64,  0.393_474_37,
+    0.397_343_84,  0.401_189_3,   0.405_011_,    0.408_809_07,  0.412_583_83,  0.416_335_46,
+    0.420_064_15,  0.423_770_13,  0.427_453_6,   0.431_114_76,  0.434_753_84,  0.438_370_97,
+    0.441_966_44,  0.445_540_4,   0.449_093_,    0.452_624_53,  0.456_135_06,  0.459_624_9,
+    0.463_094_12,  0.466_542_93,  0.469_971_57,  0.473_380_15,  0.476_768_94,  0.480_137_94,
+    0.483_487_52,  0.486_817_7,   0.490_128_73,  0.493_420_7,   0.496_693_88,  0.499_948_32,
+    0.503_184_26,  0.506_401_84,  0.509_601_2,   0.512_782_45,  0.515_945_85,  0.519_091_4,
+    0.522_219_5,   0.525_330_07,  0.528_423_25,  0.531_499_3,   0.534_558_36,  0.537_600_5,
+    0.540_625_93,  0.543_634_8,   0.546_627_04,  0.549_603_04,  0.552_562_83,  0.555_506_47,
+    0.558_434_3,   0.561_346_23,  0.564_242_5,   0.567_123_23,  0.569_988_5,   0.572_838_4,
+    0.575_673_2,   0.578_492_94,  0.581_297_7,   0.584_087_6,   0.586_862_8,   0.589_623_45,
+    0.592_369_56,  0.595_101_36,  0.597_818_9,   0.600_522_3,   0.603_211_6,   0.605_887_,
+    0.608_548_64,  0.611_196_6,   0.613_830_8,   0.616_451_56,  0.619_059_,    0.621_653_14,
+    0.624_234_,    0.626_801_85,  0.629_356_7,   0.631_898_64,  0.634_427_7,   0.636_944_2,
+    0.639_448_05,  0.641_939_34,  0.644_418_24,  0.646_884_86,  0.649_339_2,   0.651_781_4,
+    0.654_211_5,   0.656_629_74,  0.659_036_04,  0.661_430_6,   0.663_813_4,   0.666_184_66,
+    0.668_544_35,  0.670_892_6,   0.673_229_46,  0.675_555_05,  0.677_869_44,  0.680_172_74,
+    0.682_464_96,  0.684_746_2,   0.687_016_6,   0.689_276_2,   0.691_525_04,  0.693_763_3,
+    0.695_990_9,   0.698_208_03,  0.700_414_8,   0.702_611_1,   0.704_797_2,   0.706_973_1,
+    0.709_138_8,   0.711_294_5,   0.713_440_1,   0.715_575_9,   0.717_701_8,   0.719_817_9,
+    0.721_924_25,  0.724_020_96,  0.726_108_,    0.728_185_65,  0.730_253_8,   0.732_312_56,
+    0.734_361_95,  0.736_402_1,   0.738_433_1,   0.740_454_9,   0.742_467_6,
+];
--- src/apu/filter.rs
+++ src/apu/filter.rs
@@ -1,49 +1,270 @@
-use std::f64::consts::PI;
+use std::f32::consts::{PI, TAU};
 
-#[derive(Debug)]
-pub struct FirstOrderFilter {
-    b0: f64,
-    b1: f64,
-    a1: f64,
-    prev_x: f64,
-    prev_y: f64,
+#[derive(Debug, Clone, Copy)]
+#[must_use]
+pub enum FilterKind {
+    Identity,
+    HighPass,
+    LowPass,
 }
 
-impl FirstOrderFilter {
-    pub fn high_pass(sample_rate: f64, cutoff_frequency: f64) -> Self {
-        let cutoff_frequency = cutoff_frequency.min(sample_rate / 2.0);
-        // let c = sample_rate / PI / cutoff_frequency;
-        let c = sample_rate / (PI * cutoff_frequency);
-        let a0i = 1.0 / (1.0 + c);
+/// An infinite impulse response (IIR) filter.
+#[derive(Debug, Clone)]
+#[must_use]
+pub struct Iir {
+    pub alpha: f32,
+    pub prev_output: f32,
+    pub prev_input: f32,
+    pub delta: f32,
+    pub kind: FilterKind,
+}
 
+impl Iir {
+    pub const fn identity() -> Self {
         Self {
-            b0: c * a0i,
-            b1: -c * a0i,
-            a1: (1.0 - c) * a0i,
-            prev_x: 0.0,
-            prev_y: 0.0,
+            alpha: 0.0,
+            prev_output: 0.0,
+            prev_input: 0.0,
+            delta: 0.0,
+            kind: FilterKind::Identity,
         }
     }
 
-    pub fn low_pass(sample_rate: f64, cutoff_frequency: f64) -> Self {
-        let cutoff_frequency = cutoff_frequency.min(sample_rate / 2.0);
-        // let c = sample_rate / PI / cutoff_frequency;
-        let c = sample_rate / (PI * cutoff_frequency);
-        let a0i = 1.0 / (1.0 + c);
+    pub fn high_pass(sample_rate: f32, cutoff: f32) -> Self {
+        let period = 1.0 / sample_rate;
+        let cutoff_period = 1.0 / cutoff;
+        let alpha = cutoff_period / (cutoff_period + period);
 
         Self {
-            b0: a0i,
-            b1: a0i,
-            a1: (1.0 - c) * a0i,
-            prev_x: 0.0,
-            prev_y: 0.0,
+            alpha,
+            prev_output: 0.0,
+            prev_input: 0.0,
+            delta: 0.0,
+            kind: FilterKind::HighPass,
         }
     }
 
-    pub fn tick(&mut self, x: f64) -> f64 {
-        let y = self.b0 * x + self.b1 * self.prev_x - self.a1 * self.prev_y;
-        self.prev_y = y;
-        self.prev_x = x;
-        y
+    pub fn low_pass(sample_rate: f32, cutoff: f32) -> Self {
+        let period = 1.0 / sample_rate;
+        let cutoff_period = 1.0 / (TAU * cutoff);
+        let alpha = cutoff_period / (cutoff_period + period);
+        Self {
+            alpha,
+            prev_output: 0.0,
+            prev_input: 0.0,
+            delta: 0.0,
+            kind: FilterKind::LowPass,
+        }
+    }
+
+    pub fn output(&self) -> f32 {
+        match self.kind {
+            FilterKind::Identity => self.prev_input,
+            FilterKind::HighPass => self.alpha * self.prev_output + self.alpha * self.delta,
+            FilterKind::LowPass => self.prev_output + self.alpha * self.delta,
+        }
+    }
+
+    pub fn consume(&mut self, sample: f32) {
+        self.prev_output = self.output();
+        self.delta = sample - self.prev_input;
+        self.prev_input = sample;
+    }
+}
+
+/// A finite impulse response (FIR) filter.
+#[derive(Debug, Clone)]
+#[must_use]
+pub struct Fir {
+    pub kernel: Vec<f32>,
+    pub inputs: Vec<f32>,
+    pub input_index: usize,
+    pub kind: FilterKind,
+}
+
+impl Fir {
+    pub fn low_pass(sample_rate: f32, cutoff: f32, window_size: usize) -> Self {
+        Self {
+            kernel: windowed_sinc_kernel(sample_rate, cutoff, window_size),
+            inputs: vec![0.0; window_size + 1],
+            input_index: 0,
+            kind: FilterKind::LowPass,
+        }
+    }
+
+    pub fn consume(&mut self, sample: f32) {
+        self.inputs[self.input_index] = sample;
+        self.input_index += 1;
+        if self.input_index >= self.inputs.len() {
+            self.input_index = 0;
+        }
+    }
+
+    pub fn output(&self) -> f32 {
+        self.kernel
+            .iter()
+            .zip(self.inputs.iter().cycle().skip(self.input_index))
+            .map(|(k, v)| k * v)
+            .sum()
+    }
+}
+
+/// Generate a windowed sinc kernel.
+pub fn windowed_sinc_kernel(sample_rate: f32, cutoff: f32, window_size: usize) -> Vec<f32> {
+    fn blackman_window(index: usize, window_size: usize) -> f32 {
+        let i = index as f32;
+        let m = window_size as f32;
+        0.42 - 0.5 * ((TAU * i) / m).cos() + 0.08 * ((2.0 * TAU * i) / m).cos()
+    }
+
+    fn sinc(index: usize, fc: f32, window_size: usize) -> f32 {
+        let i = index as f32;
+        let m = window_size as f32;
+        let shifted_index = i - (m / 2.0);
+        if index == (window_size / 2) {
+            TAU * fc
+        } else {
+            (TAU * fc * shifted_index).sin() / shifted_index
+        }
+    }
+
+    fn normalize(input: Vec<f32>) -> Vec<f32> {
+        let sum: f32 = input.iter().sum();
+        input.into_iter().map(|x| x / sum).collect()
+    }
+
+    let fc = cutoff / sample_rate;
+    let mut kernel = Vec::with_capacity(window_size);
+    for i in 0..=window_size {
+        kernel.push(sinc(i, fc, window_size) * blackman_window(i, window_size));
+    }
+    normalize(kernel)
+}
+
+/// Represents a digital audio filter.
+#[derive(Debug, Clone)]
+#[must_use]
+pub enum Filter {
+    Iir(Iir),
+    Fir(Fir),
+}
+
+impl Filter {
+    pub fn consume(&mut self, sample: f32) {
+        match self {
+            Filter::Iir(iir) => iir.consume(sample),
+            Filter::Fir(fir) => fir.consume(sample),
+        }
+    }
+
+    pub fn output(&self) -> f32 {
+        match self {
+            Filter::Iir(iir) => iir.output(),
+            Filter::Fir(fir) => fir.output(),
+        }
+    }
+}
+
+impl From<Iir> for Filter {
+    fn from(filter: Iir) -> Self {
+        Self::Iir(filter)
+    }
+}
+
+impl From<Fir> for Filter {
+    fn from(filter: Fir) -> Self {
+        Self::Fir(filter)
+    }
+}
+
+/// Represents a filter with a given sampling period.
+#[derive(Debug, Clone)]
+#[must_use]
+pub struct SampledFilter {
+    pub filter: Filter,
+    pub sample_period: f32,
+    pub period_counter: f32,
+}
+
+impl SampledFilter {
+    pub fn new(filter: impl Into<Filter>, sample_rate: f32) -> Self {
+        Self {
+            filter: filter.into(),
+            sample_period: 1.0 / sample_rate,
+            period_counter: 0.0,
+        }
+    }
+}
+
+/// Represents a chain of filters for a given [`NesRegion`].
+#[derive(Debug, Clone)]
+pub struct FilterChain {
+    pub dt: f32,
+    pub filters: Vec<SampledFilter>,
+}
+
+impl FilterChain {
+    pub fn new(output_rate: f32) -> Self {
+        let clock_rate = 1_789_773.0;
+        let intermediate_sample_rate = output_rate * 2.0 + (PI / 32.0);
+        let intermediate_cutoff = output_rate * 0.4;
+
+        let mut filters = vec![
+            SampledFilter::new(Iir::identity(), 1.0),
+            SampledFilter::new(Iir::low_pass(clock_rate, intermediate_cutoff), clock_rate),
+        ];
+        // first-order high-pass filter at 90 Hz
+        filters.push(SampledFilter::new(
+            Iir::high_pass(intermediate_sample_rate, 90.0),
+            intermediate_sample_rate,
+        ));
+        // first-order high-pass filter at 440 Hz
+        filters.push(SampledFilter::new(
+            Iir::high_pass(intermediate_sample_rate, 440.0),
+            intermediate_sample_rate,
+        ));
+        // first-order low-pass filter at 14 kHz
+        filters.push(SampledFilter::new(
+            Iir::low_pass(intermediate_sample_rate, 14000.0),
+            intermediate_sample_rate,
+        ));
+        // TODO: Support famicom filter selection
+        // // first-order high-pass filter at 37 Hz
+        // filters.push(SampledFilter::new(
+        //     Iir::high_pass(intermediate_sample_rate, 37.0),
+        //     intermediate_sample_rate,
+        // ));
+
+        // high-quality low-pass filter
+        let window_size = 160;
+        let intermediate_cutoff = output_rate * 0.45;
+        filters.push(SampledFilter::new(
+            Fir::low_pass(intermediate_sample_rate, intermediate_cutoff, window_size),
+            intermediate_sample_rate,
+        ));
+
+        Self {
+            dt: 1.0 / clock_rate,
+            filters,
+        }
+    }
+
+    pub fn consume(&mut self, sample: f32) {
+        // Add sample to identity filter
+        self.filters[0].filter.consume(sample);
+        for i in 1..self.filters.len() {
+            let prev = i - 1;
+            let current = i;
+            while self.filters[current].period_counter >= self.filters[current].sample_period {
+                self.filters[current].period_counter -= self.filters[current].sample_period;
+                let prev_output = self.filters[prev].filter.output();
+                self.filters[current].filter.consume(prev_output);
+            }
+            self.filters[current].period_counter += self.dt;
+        }
+    }
+
+    pub fn output(&self) -> f32 {
+        self.filters.last().map_or(0.0, |f| f.filter.output())
     }
 }
